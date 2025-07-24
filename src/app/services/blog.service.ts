c63cbaa4aac8f39420a3017469f7049317cc99f1
Antony Premkumar <antonyashleyraja@gmail.com>
2025-07-24 21:51:16 +0200

    add basic content

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -4,6 +4,36 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BlogService {
+  posts = [
+    {
+      id: 'cfd-001',
+      title: 'Introduction to CFD with OpenFOAM',
+      content: `
+# Introduction
 
-  constructor() { }
+CFD is used to simulate fluid motion by solving Navier-Stokes equations:
+
+$$
+\\nabla \\cdot \\vec{u} = 0, \\quad \\rho\\left(\\frac{\\partial \\vec{u}}{\\partial t} + \\vec{u} \\cdot \\nabla \\vec{u}\\right) = -\\nabla p + \\mu \\nabla^2 \\vec{u}
+$$
+
+We use OpenFOAM for simulating incompressible flows.
+
+\`\`\`bash
+blockMesh
+simpleFoam
+\`\`\`
+      `,
+      author: 'Antony Premkumar',
+      date: '2025-07-24'
+    }
+  ];
+
+  getPosts() {
+    return this.posts;
+  }
+
+  getPostById(id: string) {
+    return this.posts.find(p => p.id === id);
+  }
 }
